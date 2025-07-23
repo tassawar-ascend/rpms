6c0bac02442dd25973f7dec27751837378a684c1
tassawar-ascend <tasawarhussain8271@gmail.com>
2025-07-23 15:53:11 +0500

    fix and changes

--- src/Http/Controllers/PermissionController.php
+++ src/Http/Controllers/PermissionController.php
@@ -6,34 +6,11 @@ use Illuminate\Routing\Controller;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Http\Request;
 
-class PermissionController extends Controller
+class PermissionHandler extends Controller
 {
-    public function index()
-    {
-        return Permission::all();
-    }
-
-    public function create(Request $request)
-    {
-        $request->validate(['name' => 'required|string']);
-        return Permission::create(['name' => $request->name]);
-    }
-
-    public function show($id)
-    {
-        return Permission::findOrFail($id);
-    }
-
-    public function edit(Request $request)
-    {
-        $permission = Permission::findOrFail($request->id);
-        $permission->update(['name' => $request->name]);
-        return $permission;
-    }
-
-    public function delete($id)
-    {
-        Permission::findOrFail($id)->delete();
-        return response()->json(['deleted' => true]);
-    }
+    public function index() { return response()->json(['data' => 'Permission Index']); }
+    public function create(Request $request) { return response()->json(['data' => 'Permission Created']); }
+    public function show($id) { return response()->json(['data' => 'Permission Show: ' . $id]); }
+    public function edit(Request $request) { return response()->json(['data' => 'Permission Updated']); }
+    public function delete($id) { return response()->json(['data' => 'Permission Deleted: ' . $id]); }
 }
---
+++ src/Http/Controllers/RoleHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace YourVendor\PackageB\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+
+class RoleHandler extends Controller
+{
+    public function index() { return response()->json(['data' => 'Role Index']); }
+    public function create(Request $request) { return response()->json(['data' => 'Role Created']); }
+    public function show($id) { return response()->json(['data' => 'Role Show: ' . $id]); }
+    public function edit(Request $request) { return response()->json(['data' => 'Role Updated']); }
+    public function delete($id) { return response()->json(['data' => 'Role Deleted: ' . $id]); }
+    public function assignPermission(Request $request) { return response()->json(['data' => 'Assigned Permission']); }
+    public function permission() { return response()->json(['data' => 'Permissions List']); }
+}
