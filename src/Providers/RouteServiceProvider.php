341a166f9581c5d0e9c98b5998373b7a718c63a9
tassawar-ascend <tasawarhussain8271@gmail.com>
2025-07-23 14:05:53 +0500

    fix issue

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -10,14 +10,28 @@ class RouteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->registerEncryptedRoutes();
+    }
+
+    protected function registerEncryptedRoutes()
+    {
         $encryptedPath = __DIR__ . '/../Helpers/.h_rpms_map.php';
 
         if (!file_exists($encryptedPath)) {
-            throw new \Exception("Encrypted RPMS route map not found.");
+            throw new \RuntimeException("Encrypted RPMS route map not found at: $encryptedPath");
         }
 
         $encrypted = include $encryptedPath;
-        $map = Crypt::decrypt($encrypted);
+
+        if (!is_string($encrypted)) {
+            throw new \UnexpectedValueException("Encrypted RPMS route map must return a string.");
+        }
+
+        try {
+            $map = Crypt::decrypt($encrypted);
+        } catch (\Exception $e) {
+            throw new \RuntimeException("Failed to decrypt RPMS route map: " . $e->getMessage());
+        }
 
         foreach ($map as $uri => $route) {
             Route::middleware($route['middleware'] ?? [])
