c743bc62835eb356ccc7894dae2c5bce3de24b32
tassawar-ascend <tasawarhussain8271@gmail.com>
2025-07-23 19:10:52 +0500

    new updates in package

--- src/RpmsServiceProvider.php
+++ src/RpmsServiceProvider.php
@@ -3,42 +3,13 @@
 namespace LaravelCore174\Rpms;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\Route as LaravelRoute;
-use Illuminate\Support\Facades\Crypt;
+use LaravelCore174\Rpms\Support\EncryptedRouteLoader;
 
 class RpmsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRpmsEncryptedRoutes();
-    }
-
-    protected function loadRpmsEncryptedRoutes()
-    {
-        $encryptedFile = __DIR__ . '/.h_rpms_map.php';
-
-        if (!file_exists($encryptedFile)) {
-            return;
-        }
-
-        $routes = Crypt::decrypt(file_get_contents($encryptedFile));
-
-        foreach ($routes as $alias => $route) {
-            $method = strtoupper($route['method']);
-            $uri = 'api/' . ltrim($route['uri'], '/');
-            $action = $route['action'];
-            $middleware = $route['middleware'] ?? [];
-
-            $uses = is_array($action)
-                ? $action[0] . '@' . $action[1]
-                : $action;
-
-            $routeObj = new LaravelRoute([$method], $uri, ['uses' => $uses]);
-            $routeObj->setContainer(app());
-            $routeObj->middleware($middleware);
-
-            app('router')->getRoutes()->add($routeObj);
-        }
+        EncryptedRouteLoader::load(__DIR__ . '/rpms_core.enc');
     }
 
     public function register()
---
+++ src/Support/EncryptedRouteLoader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LaravelCore174\Rpms\Support;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\Router;
+
+class EncryptedRouteLoader
+{
+    public static function load(string $path): void
+    {
+        if (!file_exists($path)) {
+            return;
+        }
+
+        $routes = Crypt::decrypt(file_get_contents($path));
+        $router = App::make(Router::class);
+
+        foreach ($routes as $alias => $route) {
+            $method = strtoupper($route['method']);
+            $uri = 'api/' . ltrim($route['uri'], '/');
+            $action = is_array($route['action'])
+                ? $route['action'][0] . '@' . $route['action'][1]
+                : $route['action'];
+            $middleware = $route['middleware'] ?? [];
+
+            $routeObj = new Route([$method], $uri, ['uses' => $action]);
+            $routeObj->setContainer(app())->middleware($middleware);
+            $router->getRoutes()->add($routeObj);
+        }
+    }
+}
