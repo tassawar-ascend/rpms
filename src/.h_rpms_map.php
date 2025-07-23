3cbf7b45d08cdeb8115d23d3001e3bb13bba0b82
tassawar-ascend <tasawarhussain8271@gmail.com>
2025-07-23 18:01:51 +0500

    new update

--- src/.h_rpms_map.php
+++ src/.h_rpms_map.php
@@ -1,28 +1,82 @@
 <?php
 
+use LaravelCore174\Rpms\Http\Controllers\PermissionController;
+use LaravelCore174\Rpms\Http\Controllers\RoleController;
+
 return [
-    'permissions' => [
-        'prefix' => 'r13',
-        'controller' => \LaravelCore174\Rpms\Http\Controllers\PermissionController::class,
-        'routes' => [
-            'GET /api/permissions' => 'index',
-            'POST /api/permissions/create' => 'create',
-            'GET /api/permissions/show/{id}' => 'show',
-            'POST /api/permissions/update' => 'edit',
-            'DELETE /api/permissions/delete/{id}' => 'delete',
-        ],
+    // Permissions routes
+    'r1' => [
+        'method' => 'GET',
+        'middleware' => ['api'],
+        'action' => [PermissionController::class, 'index'],
+        'uri' => 'permissions',
     ],
-    'roles' => [
-        'prefix' => 'r14',
-        'controller' => \LaravelCore174\Rpms\Http\Controllers\RoleController::class,
-        'routes' => [
-            'GET /api/role' => 'index',
-            'POST /api/role/create' => 'create',
-            'GET /api/role/show/{id}' => 'show',
-            'POST /api/role/update' => 'edit',
-            'DELETE /api/role/delete/{id}' => 'delete',
-            'POST /api/role/assignPermission' => 'assignPermission',
-            'GET /api/role/permission' => 'permission',
-        ],
+    'r2' => [
+        'method' => 'POST',
+        'middleware' => ['api'],
+        'action' => [PermissionController::class, 'create'],
+        'uri' => 'permissions/create',
+    ],
+    'r3' => [
+        'method' => 'GET',
+        'middleware' => ['api'],
+        'action' => [PermissionController::class, 'show'],
+        'uri' => 'permissions/show/{id}',
+    ],
+    'r4' => [
+        'method' => 'POST',
+        'middleware' => ['api'],
+        'action' => [PermissionController::class, 'edit'],
+        'uri' => 'permissions/update',
+    ],
+    'r5' => [
+        'method' => 'DELETE',
+        'middleware' => ['api'],
+        'action' => [PermissionController::class, 'delete'],
+        'uri' => 'permissions/delete/{id}',
+    ],
+
+    // Role routes
+    'r6' => [
+        'method' => 'GET',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'index'],
+        'uri' => 'role',
+    ],
+    'r7' => [
+        'method' => 'POST',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'create'],
+        'uri' => 'role/create',
+    ],
+    'r8' => [
+        'method' => 'GET',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'show'],
+        'uri' => 'role/show/{id}',
+    ],
+    'r9' => [
+        'method' => 'POST',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'edit'],
+        'uri' => 'role/update',
+    ],
+    'r10' => [
+        'method' => 'DELETE',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'delete'],
+        'uri' => 'role/delete/{id}',
+    ],
+    'r11' => [
+        'method' => 'POST',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'assignPermission'],
+        'uri' => 'role/assignPermission',
+    ],
+    'r12' => [
+        'method' => 'GET',
+        'middleware' => ['api'],
+        'action' => [RoleController::class, 'permission'],
+        'uri' => 'role/permission',
     ],
 ];
--- src/RpmsServiceProvider.php
+++ src/RpmsServiceProvider.php
@@ -4,7 +4,6 @@ namespace LaravelCore174\Rpms;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 class RpmsServiceProvider extends ServiceProvider
 {
@@ -16,30 +15,21 @@ class RpmsServiceProvider extends ServiceProvider
 
     protected function loadRpmsCleanRoutes()
     {
-        $mapFile = base_path('vendor/laravelcore174/rpms/.h_rpms_map.php');
+        $mapFile = __DIR__ . '/.h_rpms_map.php';
 
-        if (!file_exists($mapFile)) return;
+        if (!file_exists($mapFile)) {
+            return;
+        }
 
-        $mappings = include $mapFile;
+        $routes = include $mapFile;
 
-        foreach ($mappings as $module) {
-            $prefix = $module['prefix'];
+        foreach ($routes as $alias => $route) {
+            $method = strtolower($route['method']);
+            $uri = $route['uri'];
+            $action = $route['action'];
+            $middleware = $route['middleware'] ?? [];
 
-            foreach ($module['routes'] as $routeKey => $actionName) {
-                [$method, $uri] = explode(' ', $routeKey, 2);
-                $method = strtolower($method);
-
-                Route::$method($uri, function (...$args) use ($prefix, $actionName) {
-                    $a = base64_encode($actionName);
-                    $query = http_build_query(request()->query());
-                    $idParams = collect($args)->map(fn($v, $k) => 'id=' . $v)->implode('&');
-                    $extra = $query ? "&$query" : '';
-                    $params = $idParams . $extra;
-
-                    return redirect()->to("api/{$prefix}?a={$a}" . ($params ? "&$params" : ''))
-                                     ->withInput(request()->all());
-                });
-            }
+            Route::$method($uri, $action)->middleware($middleware);
         }
     }
 
